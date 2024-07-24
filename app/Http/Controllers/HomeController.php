459b7556ff0ba05f0372705c4f5c55d8d81f3a8e
krlos <pckrlos@gmail.com>
2024-07-24 16:59:33 -0400

    added firt template on project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function home()
+    {
+        return redirect('dashboard');
+    }
     /**
      * Display a listing of the resource.
      */
