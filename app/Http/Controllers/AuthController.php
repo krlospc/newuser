7589b9b3430bb96afccba6e9adac7cb817cd09c9
krlos <pckrlos@gmail.com>
2024-07-24 10:15:07 -0400

    added login files - step 3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
-        return view('login.login');
+        return view('auth.login');
+    }
+
+    public function loginPost(Request $request){
+
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        //dd(Hash::make('password'));
+        $credentials = $request->only('username', 'password');
+        //dd($credentials);
+        if(Auth::attempt($credentials)){
+            dd('yes');
+        }else{
+            return redirect()->intended('/');
+        }
+        
+        return view('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\HomeController;
 
 Route::group(['middleware' => 'guest'], function(){
     Route::get('/', [AuthController::class, 'login'])->name('login');
-    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login.post');
 });
 
 Route::group(['middleware' => 'auth'], function(){
